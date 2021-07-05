e6bb0a29439d676991991793400e5267b10be156
Hélio <insign@gmail.com>
2021-07-05 04:54:38 -0300

    changed default port to 8088

--- quasar.conf.js
+++ quasar.conf.js
@@ -109,7 +109,7 @@ module.exports = function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
-      port:  8080,
+      port:  8088,
       open:  true, // opens browser window automatically
     },
 
